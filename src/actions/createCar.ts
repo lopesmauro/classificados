935730df0ec6b9f85c50f592b156029c3aa34940
Mauro Felipe <mfelipxl@gmail.com>
2024-12-26 10:29:24 -0400

    Finishing the action about add a car to database.

--- src/actions/createCar.ts
+++ src/actions/createCar.ts
@@ -1,26 +1,39 @@
 "use server"
-
+import { addCar } from "@/services/car"
 import { createCarSchema } from "@/schemas/createCar"
 import { fitImage } from "@/utils/fit-image"
 import { redirect } from "next/navigation"
 
 export const createCar = async (formData: FormData) => {
-    const data = createCarSchema.safeParse({
-      title: formData.get('title'),
-      description: formData.get('description'),
-      author_name: formData.get('author_name'),
-      author_email: formData.get('author_email'),
-      price_to: formData.get('price_to'),
-      price_from: formData.get('price_from'),
-      img: formData.get('img')
+  const data = createCarSchema.safeParse({
+    img: formData.get('img'),
+    title: formData.get('title'),
+    price_to: formData.get('price_to'),
+    price_from: formData.get('price_from'),
+    negotiable: formData.get('negotiable'),
+    author_name: formData.get('author_name'),
+    author_email: formData.get('author_email'),
+    description: formData.get('description'),
+  })
+
+  if(data.success) {
+    console.log(data.data)
+    const imgName = await fitImage(data.data.img)
+    const newCar = await addCar({
+      imgName: imgName,
+      title: data.data.title,
+      priceFrom: data.data.price_from,
+      priceTo: data.data.price_to,
+      negotiable: data.data.negotiable === 'on',
+      authorName: data.data.author_name,
+      authorEmail: data.data.author_email,
+      description: data.data.description,
     })
-
-    if(data.success) {
-      console.log(data.data)
-      const imgName = await fitImage(data.data.img)
-    } else {
-      console.log(data.error.flatten().fieldErrors)
-    }
-
-    redirect('/')
+    const id = newCar.id
+    console.log(id)
+    return
+  } else {
+    console.log(data.error.flatten().fieldErrors)
+  }
+  redirect('/')
 }
